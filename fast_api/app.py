ec5b261585c1860d1fada557a10adad83f7d6060
Emerson Souza <emerson.souza@encora.com>
2024-07-10 12:52:46 -0300

    Criando rotas para aceitar outros verbos HTTP, no caso é um POST

--- fast_api/app.py
+++ fast_api/app.py
@@ -5,4 +5,8 @@ app = FastAPI()
 
 @app.get('/')
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {'message': 'GET OLÁ MUNDO!'}
+
+@app.post('/')
+def read_root():
+    return {'message': 'POST Olá Mundo!'}
