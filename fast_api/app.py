d6cf037dfd02aaab3c702928fe06d35874f5b4eb
Emerson Souza <emerson.souza@encora.com>
2024-07-16 09:48:45 -0300

    Adicionado contrato para APIs

--- fast_api/app.py
+++ fast_api/app.py
@@ -2,9 +2,11 @@ from http import HTTPStatus
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
+from fast_api.shemas import Message
+
 app = FastAPI()
 
-@app.get('/', status_code=HTTPStatus.OK) 
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message) 
 def read_root():
     return {'message': 'GET OLÁ MUNDO!'}
 
